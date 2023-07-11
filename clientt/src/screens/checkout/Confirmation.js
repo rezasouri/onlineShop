58d8786e98c39d9b6ad46d2447064e97c494a41b
Reza Souri <rezasouri654@gmail.com>
2023-07-11 16:20:11 +0330

    adding confirmation form to project

--- clientt/src/screens/checkout/Confirmation.js
+++ clientt/src/screens/checkout/Confirmation.js
@@ -1,8 +1,17 @@
-const Confirmation =()=>{
-    return(
-        <div>
-            های
-        </div>
-    )
-}
-export default Confirmation
+import { Box } from "@mui/material";
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
+
+const Confirmation = () => {
+    return (
+        <Box m="90px auto" width="80%" height="50vh">
+            <Alert severity="success">
+                <AlertTitle>Success</AlertTitle>
+                You have successfully made an Order —{" "}
+                <strong>Congrats on Making your Purchase</strong>
+            </Alert>
+        </Box>
+    );
+};
+
+export default Confirmation;
