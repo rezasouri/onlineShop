9be171580147b11e5774c0b5b12402d2d779af03
Reza Souri <rezasouri654@gmail.com>
2023-07-11 12:31:55 +0330

    adding shipping form to project and scroll bar to project

--- clientt/src/screens/checkout/Shipping.js
+++ clientt/src/screens/checkout/Shipping.js
@@ -1,8 +1,65 @@
-const Shipping =()=>{
-    return(
-        <div>
-            های
-        </div>
-    )
-}
-export default Shipping
+import { Box, Checkbox, FormControlLabel, Typography } from "@mui/material";
+import AddressForm from "./AddressForm";
+
+const Shipping = ({
+  values,
+  errors,
+  touched,
+  handleBlur,
+  handleChange,
+  setFieldValue,
+}) => {
+  return (
+    <Box m="30px auto">
+      {/* Billing Form */}
+      <Box>
+        <Typography fontSize="18px" sx={{ mb: "15px" }}>
+          اطلاعات صورت حساب
+        </Typography>
+        <AddressForm
+          type="billingAddress"
+          value={values.billingAddress}
+          errors={errors}
+          touched={touched}
+          handleBlur={handleBlur}
+          handleChange={handleChange}
+        />
+      </Box>
+      <Box>
+        <FormControlLabel
+          label="مشابه برای آدرس حمل و نقل"
+          control={
+            <Checkbox
+              defaultChecked
+              value={values.shippingAddress.isSameAddress}
+              onChange={() =>
+                setFieldValue(
+                  "shippingAddress.isSameAddress",
+                  !values.shippingAddress.isSameAddress
+                )
+              }
+            />
+          }
+        />
+      </Box>
+      {/* Shippiing Address */}
+      {!values.shippingAddress.isSameAddress && (
+        <Box>
+            <Typography fontSize="18px" sx={{ mb: "15px" }}>
+          اطلاعات صورت حساب
+        </Typography>
+        <AddressForm
+          type="shippingAddress"
+          value={values.shippingAddress}
+          errors={errors}
+          touched={touched}
+          handleBlur={handleBlur}
+          handleChange={handleChange}
+        />
+
+        </Box>
+      )}
+    </Box>
+  );
+};
+export default Shipping;
